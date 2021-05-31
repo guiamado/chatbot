6bb754157e9bbfc54b5c3d3c5547170112005d2a
guiamado <amado.guigui@gmail.com>
2021-05-30 21:46:29 -0300

    [ADD] Adicionando textos em português

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -160,7 +160,16 @@
     },
     mounted (){
       (function(d, m){
-        var kommunicateSettings = {"appId":"26488b84ef40f69390bb8a2333136d83f","popupWidget":true,"automaticChatOpenOnNavigation":true};
+        var kommunicateSettings = {
+          "appId":"26488b84ef40f69390bb8a2333136d83f",
+          "popupWidget":true,
+          "automaticChatOpenOnNavigation":true,
+          "labels": {
+            'input.message': 'Digite sua mensagem...',
+            'start.new': 'Começar uma conversa nova',
+            'conversations.title': 'Conversas'
+          },
+        };
         var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
         s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
         var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
