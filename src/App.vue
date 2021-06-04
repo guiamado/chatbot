96dce42b145560ce124852c622e89594ebb152fb
guiamado <amado.guigui@gmail.com>
2021-06-04 20:18:22 -0300

    [ADD] Adicionando layout do header responsivo

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,5 @@
 <template>
   <v-app class="background-app">
-    <!-- <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar> -->
     <Header />
     <v-main>
       <HelloWorld/>
@@ -65,6 +29,7 @@ export default {
 </script>
 <style scoped>
 .background-app {
-  background: linear-gradient(168deg, #f7f7f7 50%, #ffffff 40%) !important;
+  /* background: linear-gradient(168deg, #f7f7f7 50%, #ffffff 40%) !important; */
+  background: linear-gradient(160.39deg, #FFFFFF -6.07%, #F7F7F7 38.35%, #FFFFFF 42.78%) !important;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,71 +1,135 @@
 <template>
   <div>
-    <v-container>
-        <v-row class="text-center">
-            <v-col>
-                <v-hover
-                    v-slot="{ hover }"
-                >
-                    <v-btn
-                        tile
-                        text
-                        color="#1976d2"
-                        :class="{ 'underline-button': hover }"
-                    >
-                        Início
-                    </v-btn>
-                </v-hover>
-            </v-col>
-            <v-col>
-                <v-hover
-                    v-slot="{ hover }"
-                >
-                    <v-btn
-                        tile
-                        text
-                        color="#1976d2"
-                        :class="{ 'underline-button': hover }"
-                    >
-                        Sobre
-                    </v-btn>
-                </v-hover>
-            </v-col>
-            <v-col>
-                <v-hover
-                    v-slot="{ hover }"
-                >
-                    <v-btn
-                        tile
-                        text
-                        color="#1976d2"
-                        :class="{ 'underline-button': hover }"
-                    >
-                        Documentos
-                    </v-btn>
-                </v-hover>
-            </v-col>
-            <v-col>
-                <v-hover
-                    v-slot="{ hover }"
-                >
-                    <v-btn
-                        tile
-                        text
-                        color="#1976d2"
-                        :class="{ 'underline-button': hover }"
-                    >
-                        Contatos
-                    </v-btn>
-                </v-hover>
-            </v-col>
-        </v-row>
-    </v-container>
+    <div class="image-adjust">
+        <v-img :src="topLeftImage" :width="tamanhoImagem" :height="tamanhoImagem"></v-img>
+    </div>
+    <v-row v-if="!$vuetify.breakpoint.xsOnly" class="justify-large-screen">
+        <v-col cols="8" xl="4" lg="4" sm="12">
+            <v-container>
+                <v-row class="text-center justify-large-screen">
+                    <v-col cols="2">
+                        <v-hover
+                            v-slot="{ hover }"
+                        >
+                            <v-btn
+                                tile
+                                text
+                                color="#1976d2"
+                                :class="{ 'underline-button': hover }"
+                            >
+                                Início
+                            </v-btn>
+                        </v-hover>
+                    </v-col>
+                    <v-col cols="2">
+                        <v-hover
+                            v-slot="{ hover }"
+                        >
+                            <v-btn
+                                tile
+                                text
+                                color="#1976d2"
+                                :class="{ 'underline-button': hover }"
+                            >
+                                Sobre
+                            </v-btn>
+                        </v-hover>
+                    </v-col>
+                    <v-col cols="2" xl="2" md="3" sm="3">
+                        <v-hover
+                            v-slot="{ hover }"
+                        >
+                            <v-btn
+                                tile
+                                text
+                                color="#1976d2"
+                                :class="{ 'underline-button': hover }"
+                            >
+                                Documentos
+                            </v-btn>
+                        </v-hover>
+                    </v-col>
+                    <v-col cols="2">
+                        <v-hover
+                            v-slot="{ hover }"
+                        >
+                            <v-btn
+                                tile
+                                text
+                                color="#1976d2"
+                                :class="{ 'underline-button': hover }"
+                            >
+                                Contatos
+                            </v-btn>
+                        </v-hover>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-col>
+    </v-row>
+    <template v-if="$vuetify.breakpoint.xsOnly">
+        <div class="title-mobile">
+            Início
+        </div>
+        <div style="text-align: end;">
+            <v-app-bar-nav-icon
+                color="#12174E"
+                class="space-navbar-icon"
+                @click.stop="drawer = !drawer"
+            ></v-app-bar-nav-icon>
+        </div>
+    </template>
+    <v-navigation-drawer
+      v-model="drawer"
+      temporary
+      right
+      app
+    >
+      <v-list dense>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <!-- <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon> -->
+
+          <v-list-item-content>
+            <v-list-item-title style="color: #12174E;">{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
   </div>
 </template>
 
 <script>
 export default {
     name: 'Header',
+    data() {
+        return {
+            topLeftImage: require('@/assets/images/topLeft.svg'),
+            drawer: null,
+            items: [
+                { title: 'Início', icon: 'mdi-view-dashboard' },
+                { title: 'Sobre', icon: 'mdi-forum' },
+                { title: 'Documentos', icon: 'mdi-forum' },
+                { title: 'Contatos', icon: 'mdi-forum' },
+            ],
+        };
+    },
+    computed: {
+        tamanhoImagem() {
+            if(this.$vuetify.breakpoint.smOnly) {
+                return 170;
+            }
+            if (this.$vuetify.breakpoint.xsOnly) {
+                return 53;
+            }
+            return 250;
+        }
+    },
     methods: {
     },
 
@@ -75,5 +139,28 @@ export default {
 <style scoped>
 .underline-button {
     border-bottom: 2px solid #1976d2;
+}
+
+.justify-large-screen {
+    justify-content: center;
+}
+
+.image-adjust {
+    top: 0;
+    left: 0;
+    position: absolute;
+}
+
+.space-navbar-icon {
+    margin-top: 16px;
+    margin-right: 21px;
+}
+
+.title-mobile {
+    margin-left: 26px;
+    margin-top: 20px;
+    position: absolute;
+    font-size: 20px;
+    color: #12174E;
 }
 </style>
